63cd612071cb6097534f2206c8a7596129c55ed0
“shaked6133” <sabag975@gmail.co.il>
2024-12-08 18:15:50 +0000

    update HelloShaked.java

--- HelloShaked.java
+++ HelloShaked.java
@@ -1,5 +1,5 @@
 public class HelloShaked {
     public static void main(String[] args) {
-        System.out.println("My Name is Shaked");
+        System.out.println("My Name is Shaked The King");
     }
 }
